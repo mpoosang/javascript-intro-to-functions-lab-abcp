4cf459e966abcd277f48e46a296ef05c3414beb8
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-01-23 04:20:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function sayHiToGrandma(string) {
     return `I can/t hear you!`
   }
   
-  else string.toUpperCase() === string {
+  if string.toUpperCase() === string {
     return `YES INDEED!`
   }
   
-  else 
+  if string ===  
 }
